c9abae9a64b94d01b04fc6723d5ca221cff0d64e
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-02-26 20:01:31 +0500

    Add files via upload

---
+++ Chapter-1/app.js
@@ -0,0 +1,34 @@
+// 1. Write a script to greet your website visitor using JS alert
+// box.
+
+alert("hello World");
+swal("hello World");
+
+
+// 2. Write a script to display following message on your web
+// page:
+
+alert("Error! Please enter avalid password.");
+swal("Error! Please enter avalid password.");
+
+
+// 3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+alert("Welcome to JS Land...\n Happy Coding!");
+swal("Welcome to JS Land...\n Happy Coding!");
+
+
+// 4. Write a script to display following messages in sequence:
+
+alert("Welcome to JS Land...");
+alert("Happy Coding!");
+swal("Welcome to JS Land...");
+swal("Happy Coding!");
+
+
+// 5. Generate the following message through browser’s
+// developer console:
+
+alert("Hello I can Run JSThrough my Web Browser's Console");
+swal("Hello I can Run JSThrough my Web Browser's Console");
