f4e0c38cfb568b2a7fe669b68b6d6b8e87ef5d5a
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-03-02 20:04:07 +0500

    Add files via upload

---
+++ chapter-2-3/app.js
@@ -0,0 +1,128 @@
+// chapter-2 VARIABLES FOR STRINGS
+
+
+// 1. Declare a variable called username.
+
+// var username = "arsalan waheed";
+// console.log(username);
+
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+// var myName = "arsalan waheed";
+// console.log(myName);
+
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var titledMessage="Hello World";
+// alert(titledMessage);
+
+
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+ 
+// var name="Jhone Doe";
+// var age="15 year old";
+// var certified="Certified Mobile Application Development";
+// alert(name);
+// alert(age);
+// alert(certified);
+
+
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+
+// var show="PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(show);
+
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// var email="example@gmail.com";
+// alert("My email adress is" +" "+email);
+
+
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// var book="A smarter way to learn JavaScript";
+// alert("I'm trying to learn from the book" +" "+book);
+
+
+
+// 8. Write a script to display this in browser through JS
+
+// var browser="Yah! i can write HTML content through Java Script";
+// document.write(browser);
+
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+//  var design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+//  alert(design);
+
+// -- END --
+
+
+// Chapter-3  VARIABLES FOR NUMBERS
+
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+// var age=15;
+// alert("I am "+ 15 +" "+"years old");
+
+
+
+// 2. Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+// var visited=14;
+// alert("You have visited this site"+" "+ visited +" "+"times")
+
+
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+// var birthYear=1990;
+// document.write("MY birth year is" +" "+birthYear ,"<br>");
+// document.write("Data type of My declared variable"+" "+typeof(birthYear));
+
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var VisitorsName="John Doe"
+// var ProductTitle =" T-shirt(s)"
+// var Quantity =5
+// document.write(VisitorsName+" "+"ordered"+" "+Quantity+" "+ProductTitle+" "+"on XYZ clothing store")
+
+
+// --END --
