1d6604376d222ece7baaa56fb7dbfd00ca71094d
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-03-08 15:27:09 +0500

    Add files via upload

---
+++ Chapter-4/app.js
@@ -0,0 +1,65 @@
+// VARIABLE NAMES: LEGAL & ILLEGAL
+
+
+// 1. Declare 3 variables in one statement.
+
+// var  names="hello world";
+// let  names="hello world";
+// const names="hello world";
+
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// legal
+
+// var legalVariable ="Camel Case"
+// var legal_variable ="Snack case"
+// var legalvariable =" simple"
+// var $ ="Dollar Sign"
+// var _ ="Underscore"
+
+// illegal
+
+// var 2illegal ="number start"
+// var illegal variable ="space"
+// var ! =" special caracters"
+// var if ="reserved keyword"
+// var "string" ="string"
+
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+// document.write("<h3>“Rules for naming JS variables”</h3>","<br>");
+
+// A
+// var number="number";
+// var $="$";
+// var _="_";
+// var $my_1stVariable="$my_1st_Variable"
+// document.write("Variable names can only contain "+number + $ + _ +" and " + $my_1stVariable + " <br>");
+
+// B
+// var $name="$name";
+// var _name="_name";
+// var name="name";
+// document.write("Variables must begin with a "+$name +_name +" or "+name + "<br>");
+
+// C
+// var sensitive= "sensitive";
+// document.write("Variable names are case "+ sensitive + "<br>");
+
+// D
+// var keywords="keywords"
+// document.write("Variable names should not be JS "+ keywords);
+
+
+// -- END --
