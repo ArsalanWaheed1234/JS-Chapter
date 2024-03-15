84f2bf34a1c0377c233b953457a6d1b0f5dcf0c0
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-03-16 00:38:54 +0500

    Add files via upload

---
+++ Chapter-6-9/app.js
@@ -0,0 +1,95 @@
+// 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+
+// let user=+prompt("Enter a number");
+// console.log("The Value of a is :" + user);
+// let firstNum=++user;
+// console.log("This Value of ++a is :"+ firstNum);
+// console.log("Now the Value of is :"+ firstNum);
+// let secNum=user++;
+// console.log("This Value of a++ is :"+ user);
+// console.log("Now the Value of is :"+ user);
+// let firstNumDown=--user;
+// console.log("This Value of --a is :"+ firstNumDown);
+// console.log("Now the Value of is :"+ firstNumDown);
+// let secNumDown=user--;
+// console.log("This Value of a-- is :"+ secNumDown);
+// console.log("Now the Value of is :"+ user);
+
+
+
+// 2. What will be the output in variables a, b & result after
+// execution of the following script:
+
+// let a =2;
+// let b= 1;
+// console.log("a"+ a);
+// console.log("b"+ b);
+// var result = --a - --b + ++b + b--;
+//                 // 1-0+1+1
+// console.log(result);
+
+
+
+// Write a program that takes input a name from user &
+// greet the user.
+
+// let user=prompt("Enter your name");
+// let greet=user;
+// console.log("Hello "+greet+" Welcome");
+
+
+
+// 5. Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+
+// let table=+prompt("Enter a number", 5);
+// console.log(table + " x "+" 1 "+" = "+ table*1)
+// console.log(table + " x "+" 2 "+" = "+ table*2)
+// console.log(table + " x "+" 3 "+" = "+ table*3)
+// console.log(table + " x "+" 4 "+" = "+ table*4)
+// console.log(table + " x "+" 5 "+" = "+ table*5)
+// console.log(table + " x "+" 6 "+" = "+ table*6)
+// console.log(table + " x "+" 7 "+" = "+ table*7)
+// console.log(table + " x "+" 8 "+" = "+ table*8)
+// console.log(table + " x "+" 9 "+" = "+ table*9)
+// console.log(table + " x "+" 10 "+" = "+ table*10)
+
+
+
+// 6. Take
+// a) Take three subjects name from user and store them in 3
+// different variables.
+// b) Total marks for each subject is 100, store it in another
+// variable.
+// c) Take obtained marks for first subject from user and
+// stored it in different variable.
+// d) Take obtained marks for remaining 2 subjects from user
+// and store them in variables.
+// e) Now calculate total marks and percentage and show the
+// result in browser
+// d) Take obtained marks for remaining 2 subjects from user
+// and store them in variables.
+// e) Now calculate total marks and percentage and show the
+// result in browser like this.(Hint: user table)
+
+// let userEnglish =+prompt("Enter your English Marks");
+// let userMath =+prompt("Enter your Math Marks");
+// let userUrdu =+prompt("Enter your Urdu Marks");
+// let TotalObtainMarks=userEnglish+userMath+userUrdu;
+// let totalMarks=100;
+// let allTotalMarks=totalMarks*3;
+// let percentage=TotalObtainMarks/allTotalMarks*100;
+// document.write("<table>"+"<tr>"+"<th>"+"subject "+"</th>"+"<th>"+" Total Marks "+"</th>"+"<th>"+" Obtain Marks "+"</th>"+"<th>"+" Percentage "+"</th>"+"<br>"+
+//                          "<tr>"+"<td>"+" English"+"</td>"+"<td>"+totalMarks+"</td>"+"<td>"+userEnglish+"</td>"+"<td>"+userEnglish+"%"+"</td>"+"<br>"+
+//                          "<tr>"+"<td>"+" Math "+"</td>"+"<td>"+totalMarks+"</td>"+"<td>"+userMath+"</td>"+"<td>"+userMath+"%"+"</td>"+"<br>"+
+//                          "<tr>"+"<td>"+" Urdu "+"</td>"+"<td>"+totalMarks+"</td>"+"<td>"+userUrdu+"</td>"+"<td>"+userUrdu+"%"+"</td>"+"<br>"+
+//                          "<tr>"+"<th>"+" Result"+"</th>"+"<th>"+allTotalMarks+"</th>"+"<th>"+TotalObtainMarks+"</th>"+"<th>"+percentage +"%"+"</th>"+
+// "</table>")
+
+
+
+
