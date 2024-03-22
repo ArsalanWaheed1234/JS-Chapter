ad1de64df7df7e919905c39705e0f9b9a7c7da2a
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-03-23 00:27:59 +0500

    Add files via upload

---
+++ age-calculator/app.js
@@ -0,0 +1,26 @@
+    // two methods use in age difine 1st show pop up andn 2nd show age on age button
+    
+    
+    // function age(year , user){
+    //     user=+prompt("Enter your Birth year");
+    //     year=2024
+    //     let finalAge=year-user;
+    //     alert(finalAge);
+    // }
+    // age()
+
+
+
+
+
+
+// show age on Button
+
+    // let cal=document.getElementById("cal");
+    // function age(){
+    //        let user=+prompt("Enter your Birth year");
+    //        let year=2024
+    //         let finalAge=year-user;
+    //         cal.innerText=finalAge
+    //     }
+    //     age()
