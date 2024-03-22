aa4b460e6995e23831d56932f4d59b62f0b1136a
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-03-22 21:02:30 +0500

    Add files via upload

---
+++ Counter/app.js
@@ -0,0 +1,16 @@
+
+let val = document.getElementById("value");
+let counter=0;
+function increment(){
+    counter++;
+val.innerText=counter;
+console.log(counter)
+}
+function decrement(){
+if (counter == 0){
+    counter=0;
+}
+else{    counter--;
+    val.innerText=counter;
+    console.log(counter)}
+}
