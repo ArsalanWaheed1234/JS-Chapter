eb9eeef8d018b5e18fa3e51793cd4bb6d5ef89ff
ArsalanWaheed1234 <155485598+ArsalanWaheed1234@users.noreply.github.com>
2024-04-23 00:37:55 +0500

    Add files via upload

---
+++ Chapter-14-16/app.js
@@ -0,0 +1,59 @@
+// Declare an empty array using JS literal notation to store
+// student names in future.
+
+// let arr=[];
+// arr.push()
+// console.log(arr)
+
+
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+
+// let arr = new Array()
+// arr.push(1)
+// console.log(arr)
+
+
+// 3. Declare and initialize a strings array.
+
+// let string=["hellow world","saylani","smit","students"];
+// console.log(string);
+
+
+// 4. Declare and initialize a numbers array.
+
+// let number=[10,20,30,40,50,60,70,80,90];
+// console.log(number);
+
+
+// 5. Declare and initialize a boolean array.
+
+// let boolean=[true,false];
+// console.log(boolean);
+
+
+// 6. Declare and initialize a mixed array.
+
+// let mixedArray=["hellow world",10,true,"smit",20,false,"saylani"];
+// console.log(mixedArray);
+
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, HSC,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+
+// let mixedArray=["SSC","HSC","BSC","BS","BCOM","MS","M","Phil","PhD"];
+// document.write(`<h1>Qualifications</h1><br/>1) ${mixedArray[0]}<br/> 2) ${mixedArray[1]}<br/>3) ${mixedArray[2]}<br/>4) ${mixedArray[3]}<br/>5) ${mixedArray[4]}<br/>6) ${mixedArray[5]}<br/>7) ${mixedArray[6]}<br/>8) ${mixedArray[7]}<br/>9) ${mixedArray[8]}`);
+
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+let name=["Micheal","John","Tony"];
+let score=[320,230,420];
+
